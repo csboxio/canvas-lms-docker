036d16856cdd26a49c6c3bd2a92cfe30a6ceff6f
Ethan <ethanmccaskey2@gmail.com>
2024-09-16 12:29:51 -0700

    start script

--- gcloud-start.sh
+++ gcloud-start.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
-apt-get update
-apt-get install -y docker.io
-curl -L "https://github.com/docker/compose/releases/download/2.29.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-apt-get install -y git
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
+sudo apt-get install -y git
+
 git clone --depth 1 https://github.com/ethan-mcc/canvas-lms-docker/
 cd canvas-lms-docker
+
+sudo ufw allow 80/tcp
+sudo ufw allow 3000/tcp
+sudo ufw --force enable
+
+sudo usermod -aG docker $USER
+
+chmod +x setup.sh
+sudo ./setup.sh
+
 chmod +x run.sh
-./run.sh
+sudo ./run.sh
+
--- setup.sh
+++ setup.sh
@@ -1,2 +1,1 @@
-
-docker compose -f docker-compose.yml -f docker-compose.debian.yml -p canvas-lms-debian run --rm app bundle exec rake db:initial_setup
+docker-compose -f docker-compose.yml -f docker-compose.debian.yml -p canvas-lms-debian run --rm app bundle exec rake db:initial_setup
