d64c48e1aa4401663dcbf6766dd4e8c09f511059
Ethan <ethanmccaskey2@gmail.com>
2024-10-03 15:04:57 -0700

    prod

--- canvas/config/environments/development-local.rb
+++ canvas/config/environments/development-local.rb
@@ -1,3 +1,3 @@
-config.cache_classes = false
+config.cache_classes = true
 config.action_controller.perform_caching = true
 config.action_view.cache_template_loading = true
--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f docker-compose.debian.yml -p canvas-lms-debian run --rm app bundle exec rake db:initial_setup
+docker-compose -f docker-compose.yml -f docker-compose.debian.yml -p canvas-lms-debian run --rm app RAILS_ENV=production bundle exec rake db:initial_setup
